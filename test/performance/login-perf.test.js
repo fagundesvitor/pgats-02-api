1466934ada9200b87b011b4a66222a5ffccd6ce1
Victoria <vicmance@gmail.com>
2025-10-03 13:46:43 -0300

    Adicionando thresholds e gerando relatório em html

--- test/performance/login-perf.test.js
+++ test/performance/login-perf.test.js
@@ -2,7 +2,11 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-    iterations: 10,
+    iterations: 50,
+    thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(90)<60', 'max <70'],
+  },
 };
 
 export default function () {
