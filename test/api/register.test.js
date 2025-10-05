561fe94b62e9457401446b4828728339283134b8
Vitor Fagundes <vitorfagundes.7@gmail.com>
2025-10-05 12:27:59 -0300

    criação de testes automatizados para endpoint /users/register

--- test/api/register.test.js
+++ test/api/register.test.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('POST /users/register', () => {
+    it('Deve retornar 201 quando usuário for criado com sucesso', async () => {
+        const response = await request('http://localhost:3000')
+            .post('/users/register')
+            .set('Content-Type', 'application/json')
+            .send({
+                "username": "vitor5",
+                "password": "123456",
+                "favorecidos": ["vitor"]
+            });
+
+        expect(response.status).to.equal(201);
+        expect(response.body).to.deep.equal({
+            "username": "vitor5",
+            "favorecidos": ["vitor"],
+            "saldo": 10000
+        });
+    });
+
+    it('Deve retornar 400 quando usuário já existir', async () => {
+        const response = await request('http://localhost:3000')
+            .post('/users/register')
+            .set('Content-Type', 'application/json')
+            .send({
+                "username": "vitor",
+                "password": "123456",
+                "favorecidos": [
+                    "vitor"
+                ]
+            });
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.equal('Usuário já existe');
+    });
+});
