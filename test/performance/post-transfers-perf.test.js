9fb462133d6abe1c62e663e298f4ab51eba1bbf3
Geovana <gibson.geovana@gmail.com>
2025-10-07 20:45:59 -0300

    finalizei a estruturação do k6, realizei teste com token com ajuda do postman.

--- test/performance/post-transfers-perf.test.js
+++ test/performance/post-transfers-perf.test.js
@@ -2,28 +2,30 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 5,            
-  duration: '10s',   
+  vus: 5,
+  duration: '10s',
 };
 
 export default function () {
-  const url = 'http://localhost:3000/transfers'; 
+  const url = 'http://localhost:3000/transfers';
   const payload = JSON.stringify({
-    from: "123",
-    to: "456",
-    amount: 100
+    from: "wencha",
+    to: "vitor",
+    value: 100
   });
 
   const params = {
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer    ' // insira seu token gerado por postman aqui
+    }
   };
 
   const res = http.post(url, payload, params);
 
-  
   check(res, {
     'status é 201': (r) => r.status === 201,
-    'retorna id': (r) => r.json('id') !== undefined,
+    
   });
 
   sleep(1);
