489ff4ba4808ceabeaec8db7256d23ca1555ff2c
Geovana <gibson.geovana@gmail.com>
2025-10-06 21:56:23 -0300

    Ajustes para iniciar teste k6

--- test/api/users.test.js
+++ test/api/users.test.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 describe('GET /users', () => {
     it('Deve retornar 200 ao enviar requisição', async () => {
-        const response = await request(process.env.BASE_URL).get('/users');
+        const response = await request(process.env.BASE_URL_REST).get('/users');
         expect(response.status).to.equal(200);
     });
 });
