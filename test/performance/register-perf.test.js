33c275b41077693fc955a5ef0f2cd8057000f229
vickymance <vicmance@gmail.com>
2025-10-07 22:54:36 -0300

    Update register-perf.test.js
    Adicionando o código de teste de performance de registrar novo usuário no arquivo register-perf.test.js na main.

--- test/performance/register-perf.test.js
+++ test/performance/register-perf.test.js
@@ -1,1 +1,39 @@
-console.log("hello world");
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+const postRegisterPerf = JSON.parse(open('../fixtures/postRegisterPerf.json'));
+const RUN_ID = Date.now();
+
+export const options = {
+  stages: [
+    { duration: '5s', target: 10 },
+    { duration: '20s', target: 10 },
+    { duration: '5s', target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(90)<3000', 'max<5000'],
+  },
+};
+
+export default function () {
+  const url = 'http://localhost:3000/users/register';
+
+  const payload = {
+    username: `${postRegisterPerf.username}_${RUN_ID}_${__VU}_${__ITER}`,
+    password: postRegisterPerf.password,
+    favorecidos: postRegisterPerf.favorecidos,
+  };
+
+  const res = http.post(url, JSON.stringify(payload), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  check(res, {
+    'Validar que status é 201': (r) => r.status === 201,
+    'Validar que é um JSON': (r) =>
+      r.headers['Content-Type'] && r.headers['Content-Type'].includes('application/json'),
+  });
+
+  sleep(1);
+}
