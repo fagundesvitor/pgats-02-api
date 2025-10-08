4313bba41d3fbfa10cda4894462ffd3d1e00011e
vickymance <vicmance@gmail.com>
2025-10-07 22:56:16 -0300

    Update login-perf.test.js
    Adicionando o código de testes de performance de login do arquivo login-perf.test.js na main.

---
+++ test/performance/login-perf.test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+const postLoginPerf = JSON.parse(open('../fixtures/postLoginPerf.json'));
+
+export const options = {
+    stages: [
+        {duration: '5s', target: 10},
+        {duration: '20s', target: 10},
+        {duration: '5s', target: 0},
+    ],
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(90)<3000', 'max <5000'],
+    },
+};
+
+export default function () {
+    const url = 'http://localhost:3000/users/login';
+    console.log(postLoginPerf);
+    const payload = JSON.stringify(postLoginPerf);
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post(url, payload, params);
+    check(res, {
+        'Validar que o status é 200': (r) => r.status === 200,
+        'Validar que o tken é string': (r) => typeof (r.json().token) == 'string'
+    });
+
+    sleep(1);
+};
