6faf6daae8f226dbce5c431b28d86e1514438bd1
Abelkeveen <aabelkeveenn@gmail.com>
2025-10-09 17:48:07 -0300

    Adicionei o arquivo get-transfers e alterei o post-transfers. Crie arquivos de configuração, autentincação do k6 e apliquei variáveis de ambiente

--- test/performance/get-transfers.test.js
+++ test/performance/get-transfers.test.js
@@ -1,27 +1,33 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
-
+import http from "k6/http";
+import { sleep, check } from "k6";
+import { pegarBaseURL } from "../utils/variaveis.js";
+import { obterToken } from "../helpers/autenticacaok6.js";
 
 export const options = {
-  vus: 10, 
-  duration: '15s', 
+  stages: [
+    { duration: "5s", target: 10 },
+    { duration: "20s", target: 10 },
+    { duration: "5s", target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(90)<3000", "max<5000"],
+    http_req_failed: ["rate<0.01"],
+  },
 };
 
-const BASE_URL_REST = 'http://localhost:3000'; 
-
 export default function () {
-
-  let res1 = http.get(`${BASE_URL_REST}/transfers`);
-  check(res1, {
-    'GET /transfers status 200': (r) => r.status === 200,
+  const token = obterToken();
+  const url = pegarBaseURL() + "/transfers";
+  const params = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+  const res = http.get(url, params);
+  check(res, {
+    "Status é 200": (r) => r.status === 200,
+    "Retorno é array": (r) => Array.isArray(r.json()),
   });
-
- 
-  let res2 = http.get(`${BASE_URL_REST}/users`);
-  check(res2, {
-    'GET /users status 200': (r) => r.status === 200,
-  });
-
-  
-  sleep(1); 
+  sleep(1);
 }
--- test/performance/post-transfers-perf.test.js
+++ test/performance/post-transfers-perf.test.js
@@ -1,31 +1,37 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { pegarBaseURL } from "../utils/variaveis.js";
+import { obterToken } from "../helpers/autenticacaok6.js";
 
 export const options = {
   vus: 5,
-  duration: '10s',
+  duration: "10s",
 };
 
 export default function () {
-  const url = 'http://localhost:3000/transfers';
+  const url = pegarBaseURL() + "/transfers";
+  const token = obterToken();
+
   const payload = JSON.stringify({
     from: "wencha",
     to: "vitor",
-    value: 100
+    value: 100,
   });
 
   const params = {
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer    ' // insira seu token gerado por postman aqui
-    }
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
   };
 
   const res = http.post(url, payload, params);
 
   check(res, {
-    'status é 201': (r) => r.status === 201,
-    
+    "Status é 201": (r) => r.status === 201,
+    "Retorno contém 'from'": (r) => r.json().from === "wencha",
+    "Retorno contém 'to'": (r) => r.json().to === "vitor",
+    "Retorno contém 'value'": (r) => r.json().value === 100,
   });
 
   sleep(1);
